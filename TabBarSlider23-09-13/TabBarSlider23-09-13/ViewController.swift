fd320fcffac938b9a6e96527b2125636d74b2cc0
副山俊輔 <soeyamashunsuke@MacBook-Pro.local>
2023-09-13 22:59:42 +0900

    :sparkles: プロジェクトを作成

---
+++ TabBarSlider23-09-13/TabBarSlider23-09-13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TabBarSlider23-09-13
+//
+//  Created by 副山俊輔 on 2023/09/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
