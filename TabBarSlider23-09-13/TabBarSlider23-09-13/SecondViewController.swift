47d8c87dd7301bae57fc078bf681a52fbd756ac3
副山俊輔 <soeyamashunsuke@MacBook-Pro.local>
2023-09-15 20:59:55 +0900

    :sparkles: SecondViewControllerファイルを追加

---
+++ TabBarSlider23-09-13/TabBarSlider23-09-13/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  TabBarSlider23-09-13
+//
+//  Created by 副山俊輔 on 2023/09/14.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet private weak var resultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        resultLabel.text = ""
+    }
+
+}
